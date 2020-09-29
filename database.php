b5a63fccd653901852aa2ec52fb2b7e4db69719c
myc <mingyachung@gmail.com>
2020-09-29 11:22:29 +0100

    added image placeholders ready for inserting

--- database.php
+++ database.php
@@ -18,7 +18,10 @@ $result = $query->fetchAll();
     <div class="cocktail-wrapper">
         <?php 
             foreach($result as $cocktail){
-            echo '<div class="' . $cocktail['id'] . '"'; 
+            echo '<div class="' . 'image-' .$cocktail['id'] . '">';
+            echo '<div class="' . 'image' . '"></div>' ;
+        
+            // echo '<div class="' . $cocktail['id'] . '"'; 
             echo '<h2>' . $cocktail['name'] . '</h2>';
             echo '<p class="ingredients">' . $cocktail['ingredients'] . '</p>';
             echo '<p class="method">' . $cocktail['method'] . '</p>';
