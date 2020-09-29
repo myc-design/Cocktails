6acc7e686c36817f578450a745b28a1b572ed105
myc <mingyachung@gmail.com>
2020-09-29 10:11:46 +0100

    amended database and added divs to display content

--- database.php
+++ database.php
@@ -5,27 +5,28 @@ $db = new PDO('mysql:host=db; dbname=cocktails', 'root', 'password');
 
 // Step 2: Prepare a query
 $query = $db->prepare('SELECT * FROM `cocktails`;');
-// Sets this query (and this query only)
-// To fetch an assoc array
-// Best to do this on the db at the start
+
 $query->setFetchMode(PDO::FETCH_ASSOC);
+
 // Step 3: Execute the query
 $query->execute();
-$result = $query->fetchAll(); // Returns an assoc array of arrays
-echo '<pre>';
-// print_r($result);
-foreach($result as $cocktail){
-    print_r('<h2>' . $cocktail['name'] . '</h2>');
-    print_r('<p>' . $cocktail['ingredients'] . '</p>' );
-    print_r('<p>' . $cocktail['method'] . '</p>' );
-}
-echo '</pre>';
-
+$result = $query->fetchAll(); 
 
 ?>
 
 <section class="grid">
-<h2></h2>
-<p></p>
+    <div class="cocktail-wrapper">
+        <?php 
+            foreach($result as $cocktail){
+            echo '<div class="' . $cocktail['id'] . '"'; 
+            echo '<h2>' . $cocktail['name'] . '</h2>';
+            echo '<p class="ingredients">' . $cocktail['ingredients'] . '</p>';
+            echo '<p class="method">' . $cocktail['method'] . '</p>';
+            echo '</div>';
+            
+}
+?>
+    </div>
+
 
 </section>
